752ad11de4887355221b013355d0c96e37f264d6
Payal sahu <115584065+Payalsahuj@users.noreply.github.com>
2023-06-24 00:45:00 +0530

    Update Navbar.jsx

--- frontend/src/Component/Navbar.jsx
+++ frontend/src/Component/Navbar.jsx
@@ -1,123 +1,74 @@
-import { useState } from "react"
-import logo from "../Image/2-removebg-preview.png"
-import { useNavigate } from "react-router-dom"
-
-export function Navbar(){
-    const [state,setstate]=useState(false)
-    const navigate=useNavigate()
-    const [mobilestate,setmobilestate]=useState(false)
-    function handlehome(){
-        navigate("/dashboard")
-    }
-    return <div class="min-h-full">
-    <nav class="bg-gray-800">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 items-center justify-between">
-          <div class="flex items-center">
-            <div class="flex-shrink-0 hover:cursor-pointer"  onClick={handlehome}>
-              <img class="h-12 w-18" src={logo} alt="Your Company" />
-            </div>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
-
-                <a href="/dashboard" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                <a href="/interview" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Digital Interview</a>
-                <a href="/progress" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Progress sheet</a>
-                <a href="/material" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Study Materials</a>
-                <a href="/qa" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Q&A with us</a>
-
-              </div>
-            </div>
-          </div>
-          <div class="hidden md:block">
-            <div class="ml-4 flex items-center md:ml-6">
-              <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="sr-only">View notifications</span>
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                </svg>
-              </button>
-  
-          
-              <div class="relative ml-3">
-                <div>
-                  <button type="button" class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true" onClick={()=>setstate(!state)}>
-                    <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-                  </button>
-                </div>
-  
-               
-                <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" style={{display:state?'flex':'none',flexDirection:'column'}}>
-                 
-                  <a href="/profile" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                  <a href="/register" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Sign up</a>
-                  <a href="/logout" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Logout</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="-mr-2 flex md:hidden">
-       
-            <button type="button" class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false" onClick={()=>setmobilestate(!mobilestate)}>
-              <span class="sr-only">Open main menu</span>
-              
-              <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-           
-              <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-        </div>
+import { Navbar } from "../Component/Navbar";
+import video from "../Image/dashvedio.mp4"
+import dashbot from "../Image/dashbot.png"
+import DataDisplay from "../Component/Slidedata";
+import IconDisplay from "../Component/slideicon";
+import AOS from "aos"
+import "aos/dist/aos.css"
+import { useEffect } from "react";
+import Footer from "../Component/Footer";
+export function Dashboard(){
+  useEffect(()=>{
+    AOS.init({ duration: 1000 });
+  },[])
+    return (
+        <div>
+            <Navbar/>
+            <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-4 pb-2 sm:px-6 lg:px-8 ">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Welcome to Digitron Hub</h1>
       </div>
-  
-      <div class="md:hidden" id="mobile-menu" style={{display:mobilestate?'flex':'none',flexDirection:'column'}}>
-        <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-
-          <a href="/dashboard" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-          <a href="/interview" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Digital Interview</a>
-          <a href="/progress" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Progress Sheet</a>
-          <a href="/material" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Study Material</a>
-          <a href="/qa" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Q&A with us</a>
-        </div>
-        <div class="border-t border-gray-700 pb-3 pt-4">
-          <div class="flex items-center px-5">
-            <div class="flex-shrink-0">
-              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-            </div>
-            <div class="ml-3">
-              <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-              <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
-            </div>
-            <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span class="sr-only">View notifications</span>
-              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-              </svg>
-            </button>
-          </div>
-          <div class="mt-3 space-y-1 px-2">
-            <a href="/profile" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
-            <a href="/register" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-            <a href="/logout" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</a>
-          </div>
-        </div>
+    </header>
+    <div class="width:100% flex flex-row mx-auto my-10">
+      <div  class= "w-4/12 ml-6" >
+        <img  src={dashbot} alt="" />
       </div>
-    </nav>
-  
+      <div class="w-8/12  flex flex-row item-center justify-center">
+      <div class= "w-1/12 item-center flex flex-col justify-center">
+        <IconDisplay/>
 
-
-
-
-  
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-       
       </div>
-    </main>
-  </div>
+      <div class="w-9/12 text-left">
+      <DataDisplay/>
+      </div>
+      </div>
+    </div>
+    <br/>
+    <div class="w-full flex flex-row mx-auto"  >
+      <div class="w-full text-center m-auto" >
+      <video width={'100%'} height={'100%'}  opacity='.9' style={{ objectFit: 'cover', filter: 'brightness(60%)' }} src={video} autoPlay muted loop></video>
+      <div style={{height:'90px',position:'relative',top:'-90px',zIndex:'4',backgroundColor:'white'}}></div>
+      </div>
+    </div>
+    <div class="w-11/12 flex flex-col mx-auto border-solid border-2 py-3 px-7 br-5 mb-7" style={{boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',borderRadius:'10px'}} >
+    <h1 class="text-3xl font-bold tracking-tight text-gray-900 m-auto">About Digitron a Digital interview platform</h1>
+    <div class="w-full mt-3 flex flex-col mx-auto py-3 px-0 br-5 text-left"  >
+      <div style={{boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',padding:'20px 10px',width:'100%',borderRadius:'10px'}}>
+      <p ><b>Data Security and Privacy: </b> Since interviews involve sensitive information, DIGITRON platforms prioritize data security and privacy. They implement measures to protect the confidentiality of the interviews and comply with relevant data protection regulations.</p>
+      </div>
+      <br/>
+      <div style={{boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',padding:'20px 10px',width:'100%',borderRadius:'10px'}}>
+      <p><b>Integration with Applicant Tracking Systems:</b> Integration with GPT platforms allows for seamless data transfer and integration of interview outcomes. This ensures that interview feedback and evaluations are easily accessible and can be considered alongside other candidate information.</p>
+      </div>
+      <br/>
+      <div style={{boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',padding:'20px 10px',width:'100%',borderRadius:'10px'}}>
+      <p><b>Recording and Playback:</b> One of the advantages of DIGITRON platforms is the ability to record interviews. This feature allows interviewers to review interviews at a later time and share them with other stakeholders for evaluation and feedback. </p>
+      </div>
+      <br/>
+      <div style={{boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',padding:'20px 10px',width:'100%',borderRadius:'10px'}}>
+      <p><b>Interview Question Libraries:</b> Platforms offer a library of interview questions, categorized by industry, job role, or competency. These question banks serve as valuable resources for interviewers, helping them prepare for interviews and ensuring consistency in the interview process.</p>
+      </div>
+      <br/>
+      <div style={{boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',padding:'20px 10px',width:'100%',borderRadius:'10px'}}>
+      <p><b>Mobile Accessibility:</b> DIGITRON offer mobile accessibility, allowing candidates and interviewers to participate in interviews using their smartphones or tablets. Mobile compatibility expands accessibility and convenience for users who prefer or require mobile devices.</p>
+      </div>
+      <br/>
+      <div style={{boxShadow:' rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',padding:'20px 10px',width:'100%',borderRadius:'10px'}}>
+      <p><b>Information:</b> a digital interview platform offers a modern and efficient approach to conducting interviews, providing convenience, flexibility, and powerful features that enhance the hiring process for both employers and candidates.</p>
+      </div>
+    </div>
+    </div>
+    <Footer/>
+        </div>
+    )
 }
-
