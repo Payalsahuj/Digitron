0e030e792c202fe7640ca7d3f26b023e23f92827
KrunalGurao <krunalgurao1@gmail.com>
2023-06-23 20:31:23 +0530

    Backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,30 +3,13 @@ const express = require("express");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 const { userRoute } = require("./Routes/user.routes");
-
 const app = express();
-
-
 require("dotenv").config();
-
-
-
-
-
-
-
-
-
-
-
 app.use(express.json());
 
 
 
-
-
-
-
+app.use("/user",userRoute)
 
 
 
@@ -38,17 +21,3 @@ app.listen(process.env.port, () => {
   console.log("**************Connected to DB***************");
 });
 
-
-
-
-
-
-
-
-// ****************************************************************************************
-
-// mongoURL= mongodb+srv://krunalgurao:krunalgurao@digitron.zojf4xp.mongodb.net/Digitron?retryWrites=true&w=majority
-// port = 0619
-
-
-// *********************************************************************************************
