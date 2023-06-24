f34009f2c31abd6ccd1a3efa88b6f25ce4a243b3
Payalsahuj <115584065+Payalsahuj@users.noreply.github.com>
2023-06-24 11:12:17 +0530

    interview dash

--- frontend/src/Component/Navbar.jsx
+++ frontend/src/Component/Navbar.jsx
@@ -13,7 +13,7 @@ export function Navbar(){
         navigate("/dashboard")
     }
     return <div class="min-h-full">
-    <nav class="bg-gray-800">
+    <nav class="bg-gray-800 fixed top-0 left-0 w-full z-50">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
@@ -116,10 +116,10 @@ export function Navbar(){
 
 
 
-    <main>
+    {/* <main>
       <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
        
       </div>
-    </main>
+    </main> */}
   </div>
 }
--- frontend/src/Route/Allroutes.jsx
+++ frontend/src/Route/Allroutes.jsx
@@ -7,6 +7,7 @@ import { Dashboard } from "../Pages/Dashboard";
 import { StudyMaterial } from "../Pages/StudyMaterial";
 
 import QandA from "../Pages/QandA";
+import { DigitalInterview } from "../Pages/Digitalinterview";
 
 export function Allroutes() {
     return <Routes>
@@ -16,6 +17,6 @@ export function Allroutes() {
         <Route path="/register" element={<Registration />} />
       <Route path="/login" element={<Login />} />
       <Route path="/qa" element={<QandA />}></Route>
-
+      <Route path="/interview" element={<DigitalInterview/>}/>
     </Routes>
 }
