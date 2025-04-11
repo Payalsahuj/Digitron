f4eef78554c3213e9a138fedc89c7efc3363dad1
Payal sahu <115584065+Payalsahuj@users.noreply.github.com>
2025-04-12 00:19:48 +0530

    Update QandA.jsx

--- frontend/src/Pages/QandA.jsx
+++ frontend/src/Pages/QandA.jsx
@@ -25,7 +25,7 @@ export default function QandA() {
     console.log(msg);
     setLoading(true);
     axios
-      .post("https://digitron-backend.onrender.com/chat/generalChat", {
+      .post("https://digitron.onrender.com/chat/generalChat", {
         msg,
       })
       .then((res) => {
@@ -45,9 +45,7 @@ export default function QandA() {
     return <p>{value}</p>;
   };
 
-  useEffect(() => {
-    console.log("refresh");
-  }, []);
+ 
   return (
     <>
       <Navbar />
